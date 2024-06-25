c4b5a3f4932cbf42947ff31091ac98594d1dd545
Maxime Danic <maxime.danic@gmail.com>
2024-06-26 02:30:25 +0300

    feat: Add DetailPage and ErrorPage components, update routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout.jsx';
 import AboutPage from './components/pages/About/AboutPage.jsx';
+import DetailPage from './components/pages/DetailPage/DetailPage.jsx';
+import ErrorPage from './components/pages/Error/ErrorPage.jsx';
 import HomePage from './components/pages/Home/HomePage.jsx';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
+          <Route path="detail/:id" element={<DetailPage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
