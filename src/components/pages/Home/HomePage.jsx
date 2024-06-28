483935f382f1af3dd09f539b7afe366c29e0dd09
Maxime Danic <maxime.danic@gmail.com>
2024-06-28 17:48:53 +0300

    feat: Add ListAccomodations component to HomePage

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,4 +1,11 @@
+import ListAccomodations from './ListAccomodations.jsx';
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <ListAccomodations />
+    </div>
+  );
 };
 export default HomePage;
