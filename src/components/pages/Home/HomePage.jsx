7db6aafbb87e0e0a5a76664be5011148f85ba9cd
Maxime Danic <maxime.danic@gmail.com>
2024-06-28 19:12:08 +0300

    feat: Add Banner component to HomePage

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import ListAccomodations from './ListAccomodations.jsx';
+import Banner from '../../reusable-UI/Banner.jsx';
 
 const HomePage = () => {
   return (
     <div>
       Home
+      <Banner />
       <ListAccomodations />
     </div>
   );
