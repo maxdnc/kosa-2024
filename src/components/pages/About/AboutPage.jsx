2f315bd768fbb1e514c7decacc7d2d1be23ff45d
Maxime Danic <maxime.danic@gmail.com>
2024-06-28 19:11:28 +0300

    feat: Add Banner component to AboutPage

--- src/components/pages/About/AboutPage.jsx
+++ src/components/pages/About/AboutPage.jsx
@@ -1,4 +1,10 @@
+import Banner from '../../reusable-UI/Banner.jsx';
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About
+      <Banner />
+    </div>
+  );
 };
 export default AboutPage;
