e56e18eba6f3a890667abda2a1b061a9dfc8318f
Maxime Danic <maxime.danic@gmail.com>
2024-06-28 17:48:49 +0300

    feat: Add accommodation details to DetailPage

--- src/components/pages/DetailPage/DetailPage.jsx
+++ src/components/pages/DetailPage/DetailPage.jsx
@@ -1,10 +1,21 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import accommodationsData from '../../../api/accommodations.json';
 
 const DetailPage = () => {
   const { id } = useParams();
+  const accommodationData = accommodationsData.find(
+    (accommodation) => accommodation.id === id
+  );
+  const { title, cover, description } = accommodationData;
+
+  console.log(accommodationData);
   return (
     <div>
+      <Link to="/">Back to Home</Link>
       <h1>Detail Page for ID: {id}</h1>
+      <h2>{title}</h2>
+      <img src={cover} alt={title} />
+      <p>{description}</p>
     </div>
   );
 };
