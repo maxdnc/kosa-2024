56e6efeafa292c21d84c452c271fb5e6a71c317c
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 12:04:53 +0300

    feat: Update AboutPage to use PictureBanner for the Banner component

--- src/components/pages/About/AboutPage.jsx
+++ src/components/pages/About/AboutPage.jsx
@@ -1,9 +1,13 @@
 import Banner from '../../reusable-UI/Banner.jsx';
+import PictureBanner from '../../../../public/images/banner/PictureAboutPage.png';
+import styles from '../../../styles/pages/About/AboutPage.module.scss';
 const AboutPage = () => {
   return (
-    <div>
-      About
-      <Banner />
+    <div className={styles.aboutPage}>
+      <Banner
+        srcImage={PictureBanner}
+        altImage={'Paysage marin brumeux avec vagues'}
+      />
     </div>
   );
 };
