d47d7d08d78530a83e17942df0dd966f6e21f8d8
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 12:04:59 +0300

    refactor: Update Banner component to use styles module and add image support

--- src/components/reusable-UI/Banner.jsx
+++ src/components/reusable-UI/Banner.jsx
@@ -1,4 +1,11 @@
-const Banner = () => {
-  return <div>Banner</div>;
+import styles from '../../styles/components/reusable-UI/Banner.module.scss';
+
+const Banner = ({ srcImage, title, altImage }) => {
+  return (
+    <div className={`${styles.banner} ${title ? styles.opacityVariable : ''}`}>
+      {title && <span className={styles.title}>{title}</span>}
+      <img className={styles.image} src={srcImage} alt={altImage}></img>
+    </div>
+  );
 };
 export default Banner;
