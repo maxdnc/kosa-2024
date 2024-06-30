f80e3a52d47ed62a0c0ec2eb435bafd501178541
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 12:05:08 +0300

    refactor: Update Footer component to include Kasa logo and copyright text

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,4 +1,18 @@
+import KasaLogo from '../../../assets/svg/KasaLogo.jsx';
+import styles from '../../../styles/components/Layout/Footer/Footer.module.scss';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <div className={styles.wrapperLogo}>
+          <KasaLogo color={'white'} />
+        </div>
+        <div className={styles.wrapperText}>
+          <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
