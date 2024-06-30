b5dfc8e38aae9790c56afb71bd1e57922bfec226
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 13:07:48 +0300

    style: Update CardAccomodation component styling and use styles module for consistent styling

--- src/components/reusable-UI/CardAccomodation.jsx
+++ src/components/reusable-UI/CardAccomodation.jsx
@@ -1,14 +1,11 @@
 import { Link } from 'react-router-dom';
+import styles from '../../styles/components/reusable-UI/CardAccomodation.module.scss';
 
 const CardAccomodation = ({ title, coverImgSrc, id }) => {
   return (
-    <Link
-      className="card"
-      style={{ backgroundColor: 'pink', margin: '1rem' }}
-      to={`/detail/${id}`}
-    >
-      <img src={coverImgSrc} alt={title} className="card-cover" />
-      <p>{title}</p>
+    <Link className={styles.cardAccomodation} to={`/detail/${id}`}>
+      <img src={coverImgSrc} alt={title} className={styles.img} />
+      <span className={styles.title}>{title}</span>
     </Link>
   );
 };
