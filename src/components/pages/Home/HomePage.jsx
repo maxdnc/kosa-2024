b47c9ae5fce2764de6bb477de1648c8c233dce25
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 17:21:29 +0300

    refactor: Update HomePage component to use fragment instead of div

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,18 +1,17 @@
 import ListAccomodations from './ListAccomodations.jsx';
 import Banner from '../../reusable-UI/Banner.jsx';
 import PictureBanner from '../../../../public/images/banner/PictureHomePage.png';
-import styles from '../../../styles/pages/Home/HomePage.module.scss';
 
 const HomePage = () => {
   return (
-    <div className={styles.homePage}>
+    <>
       <Banner
         srcImage={PictureBanner}
         altImage={'Paysage marin brumeux avec vagues'}
         title={'Chez vous, partout et ailleurs'}
       />
       <ListAccomodations />
-    </div>
+    </>
   );
 };
 export default HomePage;
