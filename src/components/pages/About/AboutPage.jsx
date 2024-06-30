bc9bf9a9f94ef0a50307f1682ac2f8d643ccfc7d
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 14:11:00 +0300

    refactor: Update AboutPage component with Accordion

--- src/components/pages/About/AboutPage.jsx
+++ src/components/pages/About/AboutPage.jsx
@@ -1,6 +1,8 @@
-import Banner from '../../reusable-UI/Banner.jsx';
 import PictureBanner from '../../../../public/images/banner/PictureAboutPage.png';
 import styles from '../../../styles/pages/About/AboutPage.module.scss';
+import Accordion from '../../reusable-UI/Accordion.jsx';
+import Banner from '../../reusable-UI/Banner.jsx';
+
 const AboutPage = () => {
   return (
     <div className={styles.aboutPage}>
@@ -8,6 +10,8 @@ const AboutPage = () => {
         srcImage={PictureBanner}
         altImage={'Paysage marin brumeux avec vagues'}
       />
+
+      <Accordion label={'open'} description={'lodedzeddokd  deddezdé'} />
     </div>
   );
 };
