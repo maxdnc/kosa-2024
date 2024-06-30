5c4c125ca3ed21e6d86838ec0ce3b9e91daab930
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 12:05:03 +0300

    refactor: Update HomePage to use PictureBanner for the Banner component

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,11 +1,16 @@
 import ListAccomodations from './ListAccomodations.jsx';
 import Banner from '../../reusable-UI/Banner.jsx';
+import PictureBanner from '../../../../public/images/banner/PictureHomePage.png';
+import styles from '../../../styles/pages/Home/HomePage.module.scss';
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-      <Banner />
+    <div className={styles.homePage}>
+      <Banner
+        srcImage={PictureBanner}
+        altImage={'Paysage marin brumeux avec vagues'}
+        title={'Chez vous, partout et ailleurs'}
+      />
       <ListAccomodations />
     </div>
   );
