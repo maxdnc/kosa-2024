26095cbb1f79d634a913345c11721e66ae75039d
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 17:00:58 +0300

    feat: Add StarRating component for displaying star ratings

---
+++ src/components/reusable-UI/StarRating.jsx
@@ -0,0 +1,17 @@
+import StarIcon from '../../assets/svg/StarIcon.jsx';
+import styles from '../../styles/components/reusable-UI/StarRating.module.scss';
+
+const StarRating = ({ rating }) => {
+  const totalStars = 5;
+  const ratingNumber = parseInt(rating) || 0;
+
+  return (
+    <div className={styles.starRating}>
+      {[...Array(totalStars)].map((_, index) => (
+        <StarIcon key={index} isRated={index < ratingNumber} />
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
