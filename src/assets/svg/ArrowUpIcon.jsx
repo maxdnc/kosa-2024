df0b41f3334a13e5155f67c31cdb95c087770de3
Maxime Danic <maxime.danic@gmail.com>
2024-06-30 17:06:18 +0300

    Update ArrowUpIcon dimensions for consistency

--- src/assets/svg/ArrowUpIcon.jsx
+++ src/assets/svg/ArrowUpIcon.jsx
@@ -2,8 +2,8 @@ const ArrowUpIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="32"
+      height="32"
       viewBox="0 0 32 32"
       fill="none"
     >
